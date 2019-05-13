1bd0d7fa35148eaec16791202fb234461c67aa45
Scott Reyell <smreyell@gmail.com>
2019-05-13 12:41:35 -0700

    first api call successful

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
 ////////////////////////////////////
 /////// Controller
+import axios from 'axios';
+
+// URL : https://www.food2fork.com/api/search
+// API KEY Food2Fork - f42946af5e7d8514c9c65074da2c433d
+
+async function getResults(query){
+  const proxy = 'https://crossorigin.me/';
+  const URL = 'https://www.food2fork.com/api/search';
+  const API_KEY = 'f42946af5e7d8514c9c65074da2c433d';
+
+  try {
+    const res = await axios(`${URL}?key=${API_KEY}&q=${query}`)
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch(err) {
+    alert(err);
+  }
+};
+
+getResults('pasta chicken');

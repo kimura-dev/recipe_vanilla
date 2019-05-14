ef0e5ca77ff5f2fd05ba4130ba696c3cdf6943f3
Scott Reyell <smreyell@gmail.com>
2019-05-14 11:19:05 -0700

    built out part 1 of the searchView, and methods clearInput, clearResults, renderRecipe and renderResults.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 ////////////////////////////////////
 /////// Controller
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 /** Global state of the App
  * -Search Object
  * -Current recipe object
@@ -11,22 +13,23 @@ const state = {};
 
 const controlSearch = async () => {
   // 1) Get query from the view
-  const query = 'pizza' // TODO
+  const query = searchView.getInput();  
 
   if(query){
     // 2) New search object and add it to state
     state.search = new Search(query);
     // 3) Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results on UI
-    console.log(state.search.result);
+    searchView.renderResults(state.search.result)
   }
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });

42ead74e2e62ebf3d1e5282d01d9fc591c939dd1
Scott Reyell <smreyell@gmail.com>
2019-05-14 11:44:38 -0700

    added a private func to searchView to truncate the titles of the recipes. limitRecipeTitle takes in a recipe title and a limit of 17 chars.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,6 +11,31 @@ export const clearResults = () => {
   elements.searchResultList.innerHTML = '';
 };
 
+// Private Function
+//  'Pasta with tomato and spinach'
+// acc: 0 /acc + current.length = 5 / newTitle = ['Pasta']
+// acc: 5 /acc + current.length = 9 / newTitle = ['Pasta', 'with']
+// acc: 9 /acc + current.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+// acc: 15 /acc + current.length = 18 / newTitle = ['Pasta', 'with', 'tomato'] BREAK;
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+
+  if(title.length > limit){
+    title.split(' ').reduce((acc, current) => {
+      if(acc + current.length <= limit){
+        newTitle.push(current);
+      }
+
+      return acc + current.length;
+
+    }, 0);
+
+    return `${newTitle.join(' ')}...`
+  }
+
+  return title;
+};
+
 export const renderRecipe = recipe => {
   const markup = `
       <li>
@@ -19,7 +44,7 @@ export const renderRecipe = recipe => {
                   <img src="${recipe.image_url}" alt="${recipe.title}">
               </figure>
               <div class="results__data">
-                  <h4 class="results__name">${recipe.title}</h4>
+                  <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                   <p class="results__author">${recipe.publisher}</p>
               </div>
           </a>
